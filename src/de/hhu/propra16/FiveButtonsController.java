dda816b2540a1327852737a8c9a53d84a82cb271
toadie223 <jusch196@hhu.de>
2016-06-13 11:49:21 +0200

    Button E implementiert

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,37 @@
 package de.hhu.propra16;
 
-/**
- * Created by Tobias Witt on 06.06.16.
- */
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.time.LocalDateTime;
+
 public class FiveButtonsController {
+
+    @FXML
+    private Label time;
+
+    public void buttonA(){
+
+        Label secondlabel = new Label("Too Many Buttons");
+
+        StackPane secondpane = new StackPane();
+        secondpane.getChildren().add(secondlabel);
+
+        Scene secondscene = new Scene(secondpane, 200, 100);
+
+        Stage secondstage = new Stage();
+        secondstage.setTitle("Fehler - Hinweis");
+        secondstage.setScene(secondscene);
+
+        secondstage.show();
+
+    }
+
+    public void buttonE(){
+        LocalDateTime timer = LocalDateTime.now();
+        time.setText("Uhrzeit: " + timer.toString());
+    }
 }
